f8116a1ff56b8ba32a868e5a400800486e073d54
repo19352 <163495197+repo19352@users.noreply.github.com>
2024-03-27 20:16:55 +0530

    Update owasp.groovy

--- vars/owasp.groovy
+++ vars/owasp.groovy
@@ -1,1 +1,4 @@
-
+def call() {
+                  dependencyCheck additionalArguments: '--scan target/',odcInstallation: 'owasp'
+                    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+}
